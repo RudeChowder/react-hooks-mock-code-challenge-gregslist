76f4b35a0681ff4680ad233f2e12049483bbf59f
Stephen Crane <scrane88@gmail.com>
2021-07-20 13:46:13 -0400

    First deliverable - display all listings

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+
+  const listingsUrl = "http://localhost:6001/listings"
+
+  useEffect(() => {
+    fetch(listingsUrl)
+      .then(resp => resp.json())
+      .then(data => setListings(data))
+  }, [])
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings}/>
     </div>
   );
 }
